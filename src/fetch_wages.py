a54b177a6765be43a660ef6a0020e52cbdb23c9c
DimChatz-data-ml <dimchatzo91@gmail.com>
2026-07-29 11:30:50 +0300

    Add idempotency check and transaction logic to wages ingestion script

--- src/fetch_wages.py
+++ src/fetch_wages.py
@@ -4,27 +4,42 @@ from dotenv import load_dotenv
 from pyjstat import pyjstat
 import polars as pl
 import logging
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine,text
 
 
 logging.basicConfig(level=logging.INFO)
 load_dotenv()
 
-url='https://ec.europa.eu/eurostat/api/dissemination/statistics/1.0/data/nama_10_fte?format=JSON&lang=EN&geo=DE&geo=EL&geo=PL&sinceTimePeriod=2010'
-
-result=pyjstat.Dataset.read(url)
-df_pandas=result.write('dataframe')
-final_df=pl.from_pandas(df_pandas)
-
-
-logging.info(f"fetched wages data: {final_df.shape[0]} rows ")
-
 password = os.getenv("POSTGRES_PASSWORD")
 db_name = os.getenv("POSTGRES_DB")
 database_url = f"postgresql://postgres:{password}@localhost:5433/{db_name}"
-
-
 engine = create_engine(database_url)
-final_df.write_database(table_name="raw_wages", connection=engine, if_table_exists="replace")
-logging.info("Data written to raw_wages table")
+
+
+url='https://ec.europa.eu/eurostat/api/dissemination/statistics/1.0/data/nama_10_fte?format=JSON&lang=EN&geo=DE&geo=EL&geo=PL&sinceTimePeriod=2010'
+
+
+try:
+    with engine.connect() as conn:
+        counting=conn.execute(text('select count(*) from raw_wages')).scalar()
+except Exception:
+    counting=0        
+
+if counting>0:
+        logging.info('we have data already,skip')
+else:
+    result=pyjstat.Dataset.read(url)
+    df_pandas=result.write('dataframe')
+    final_df=pl.from_pandas(df_pandas)
+    logging.info(f"fetched wages data: {final_df.shape[0]} rows ")
+    with engine.begin() as conn:
+        final_df.write_database(table_name="raw_wages", connection=conn, if_table_exists="fail")
+    logging.info("Data written to raw_wages table")
+ 
+
+
+
+
+
+
 
